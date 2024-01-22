d865b2744b3538052668a3814fd9e93cf9b5cde5
LyovaMinasyan <lyova.minasyan.y@tumo.org>
2024-01-22 21:30:39 +0400

    front to back

--- public/script.js
+++ public/script.js
@@ -5,13 +5,14 @@ function send(){
   let ageInput = document.getElementById("age").value
   let emailInput = document.getElementById("email").value
   let dateInput = document.getElementById("date").value
+  let passwordInput = document.getElementById("password").value
  
   fetch('/addName', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json;charset=utf-8'
     },
-    body: JSON.stringify({fname : fnameInput, lname : lnameInput, age : ageInput, email : emailInput, date : dateInput,})
+    body: JSON.stringify({fname : fnameInput, lname : lnameInput, age : ageInput, email : emailInput, date : dateInput,password :passwordInput})
   });
 
 
--- server.js
+++ server.js
@@ -5,6 +5,16 @@ var app = express();
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
+const mongoose = require('mongoose');
+// Replace the connection string with your MongoDB connection string
+const connectionString = 'mongodb+srv://lyovaminasyan:lyov2126@cluster0.yhxeiwi.mongodb.net/sample_mflix';
+// Connect to MongoDB
+// mongoose.connect(connectionString, { useUnifiedTopology: true });
+// Check the connection
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'Connection error:'));
+
+
 
 
 app.get("/", function(req, res){
@@ -12,22 +22,98 @@ app.get("/", function(req, res){
 
 });
 
-app.post('/addName', (req, res) => {
+app.post('/addName', async (req, res) => {
     const fname = req.body.fname;
+    const password = req.body.password;
+    const email = req.body.email;
     const lname = req.body.lname;
     const age = req.body.age;
-    const email = req.body.email;
-    const date = req.body.date;
-   
-    console.log('Received data:', fname ,lname, age , email, date );
-   res.redirect("/");
- }); 
- 
+    mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'Connection error:'));
+    db.once('open', async () => {
+        console.log('Connected to MongoDB!');
+        try {
+           let result = await mongoose.connection.db.collection('users').insertOne({
+                name: fname,
+                lname: lname,
+                email: email,
+                password: password,
+                age: age,
+
+            })
+            res.json(result);
+        } catch (error) {
+            console.error('Error retrieving movies:', error);
+        } finally {
+            mongoose.connection.close();
+        }
+    })
+ });
 
 
-app.listen(3000, function(){
-   console.log("Example is running on port 3000");
-});
+
+ app.listen(3000, function(){
+    console.log("Example is running on port 3000");
+ });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// db.once('open', async() => {
+//     console.log('Connected to MongoDB!');
+//     try {
+//         app.post('/addName', async(req, res) => {
+//             const fname = req.body.fname;
+//             const lname = req.body.lname;
+//             const age = req.body.age;
+//             const email = req.body.email;
+//             const date = req.body.date;
+//             const password= req.body.password;
+//             var result = await mongoose.connection.db.collection('users').insertOne(
+//                  {
+//                     name : fname,
+//                     email : email,
+//                     password : password,
+//                 }  
+//                )
+//             console.log(result);
+//             console.log('Received data:', fname ,lname, age , email, date );
+//            res.redirect("/");
+        
+           
+//          }); 
+        
+//          }catch (error) {
+//         console.error('Error retrieving movies:', error);
+//         } finally {
+//         mongoose.connection.close();
+//         }
+    
+//         //toArray() find({ $or :[{"location.address.city":"California" }, {"location.address.city" : "Long Beach"} ] }
+    
+    
+//     // You can add additional code here for testing or other operations
+//     // Make sure to close the connection when you're done
+//     mongoose.connection.close();
+//     });
+
 
 
 
@@ -35,36 +121,14 @@ app.listen(3000, function(){
 
 
 
-const mongoose = require('mongoose');
-
-// Replace the connection string with your MongoDB connection string
-const connectionString = 'mongodb+srv://lyovaminasyan:lyov2126@cluster0.yhxeiwi.mongodb.net/?retryWrites=true&w=majority';
-
-
-// Connect to MongoDB
-mongoose.connect(connectionString, { useUnifiedTopology: true });
-
-// Check the connection
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Connection error:'));
-db.once('open', () => {
-console.log('Connected to MongoDB!');
-
-// You can add additional code here for testing or other operations
-// Make sure to close the connection when you're done
-mongoose.connection.close();
-});
 
 
 
-// var MongoClient = require('mongodb').MongoClient;
-// var url = "Cluster0";
 
-// MongoClient.connect(url, function(err, db) {
-//   if (err) throw err;
-//   console.log("Database created!");
-//   db.close();
-// });
+
+
+
+
 
 
 
